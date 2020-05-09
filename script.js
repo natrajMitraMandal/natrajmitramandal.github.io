649d88bad6be75b19d7553eeba2dd66a3085c073
ayush_dedhia25 <ayushsproductions@gmail.com>
2020-05-09 20:15:58 +0530

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-
 $(document).ready(function() {
-	alert("नटराज नवरात्र उत्सव मंडळ मध्ये तुमचे स्वागत आहे \n🌠🌠🌠");
+	alert("नटराज नवरात्र उत्सव मंडळ मध्ये तुमचे स्वागत आहे\n🌠🌠🌠");
+
+	const shareContext = {
+		title: 'Natraj Navratri Utsav Mandal',
+		text: 'Natraj Utsav Mandal',
+		url: 'https://natrajmitramandal.github.io/'
+	};
+
+	$("#share-btn").click(async function() {
+		try {
+			await navigator.share(shareContext);
+			alert("Share successful.");
+		} catch(err) {
+			alert("Something went wrong.");
+		}
+	});
+
 });
